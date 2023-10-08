276a500a18860228aacf66a7e68db0aad2abf2e8
tiemyk <tiemichaels1@gmail.com>
2023-10-08 16:40:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
 * main - A c program that prints a line to the standard error
-* Return: Always 1 (Success)
+* Return 1 (Success)
 */
 int main(void)
 {
