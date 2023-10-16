ea3c30b198aee3ff48c71af24890a561f0779408
tiemyk <tiemichaels1@gmail.com>
2023-10-16 05:28:45 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  _strlen - return the lengh of a string
+ *  @s: string
+ *  Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	return (count);
+}
